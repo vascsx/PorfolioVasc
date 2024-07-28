be9cb43882ff2bc38029325b09d3b557e0e8b353
vascsx <vasczn@gmail.com>
2024-07-28 01:54:21 -0300

    feat: adição das configurações iniciais do tailwind e estruturação da navbar

---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
+
+const Header = () => {
+  return (
+    <header className = "py-8 xl:py-12 text-white">
+        <div class = "container mx-auto flex justify-between items-center">
+            {/*logo*/}
+            <Link href = "">
+                <h1 className = "text-4xl font-semibold">
+                    Vasc <span className = "text-accent"> .</span>
+                </h1> 
+            </Link>    
+
+            {/*desktop nav*/}
+            <div className ="hidden xl:flex items-center gap-8">
+                <Nav/> 
+                <Link href = "/contact">
+                    <Button>Hire me</Button>
+                </Link>
+            </div>
+
+            {/*mobile*/}
+            <div className = "xl:hidden">mobile nav</div>    
+        </div>
+        </header>
+  )
+}
+
+export default Header
